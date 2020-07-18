0f1f780c097f28459c22d491f1af5d0a6e3995fb
tomoyd <ydh9419@163.com>
2020-07-18 21:58:26 +0800

    2.0.3

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 /**
  * 
  * @param {string} string 字符串
+ * @returns 返回一个去掉空格的字符串
  */
-module.exports = function tiny(string) {
+export function tiny(string) {
   if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
   return string.replace(/\s/g, "");
 };
